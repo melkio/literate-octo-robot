94e1c13db0cf86cc1946533d94949581ce63e3d4
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2019-10-03 20:55:18 +0200

    Saturn.HealthController.Get endpoint

--- src/Saturn/Program.cs
+++ src/Saturn/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Saturn
 {
@@ -6,7 +7,12 @@ namespace Saturn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
