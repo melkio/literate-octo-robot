c4413926cec2bd220efa53bbaaa57a03a9482585
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2019-10-03 20:41:49 +0200

    add Saturn (empty) project

---
+++ src/Saturn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
